0fb8b7b95a6ebb02d4cab60115ca934b10225d79
Abdullah <abdullah.mohamed1047@gmail.com>
2024-08-01 18:37:45 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('best_rf_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return "Hello from first model"
+
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0',port=5001)
